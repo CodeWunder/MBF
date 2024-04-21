3f3cf6175da163ee5cc22d6ae70e5e321eda03d0
Iwara Wisdom <92611753+CodeWunder@users.noreply.github.com>
2024-04-21 12:26:09 +0100

    Update CheckoutForm.js

--- src/routes/checkout/CheckoutForm.js
+++ src/routes/checkout/CheckoutForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { usePaystackPayment } from "react-paystack";
 import ResetLocation from "../../helpers/ResetLocation";
 import PaymentSuccessModal from "./PaymentSuccessModal";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 const CheckoutForm = ({ totalPayment, productsQuantity }) => {
   const [deliveryOption, setDeliveryOption] = useState("normal");
@@ -68,7 +68,6 @@ const CheckoutForm = ({ totalPayment, productsQuantity }) => {
 
   return (
     <section className="checkout-personal-information">
-      <ToastContainer />
       <PaymentSuccessModal
         isOpen={isPaymentSuccessModalOpen}
         onClose={() => setIsPaymentSuccessModalOpen(false)}
